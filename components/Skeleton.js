832b7106c2af431ae61a60ee2c44072b50c0304d
Jarmo Lindström <yaramodesu@gmail.com>
2021-12-26 10:52:09 +0200

    fallback

---
+++ components/Skeleton.js
@@ -0,0 +1,11 @@
+export default function Skeleton() {
+	return (
+		<div className="skeleton">
+			<div className="s-banner" />
+			<div className="s-header" />
+			<div className="s-content" />
+			<div className="s-content" />
+			<div className="s-content" />
+		</div>
+	);
+}
